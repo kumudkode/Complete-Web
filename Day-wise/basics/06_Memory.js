1a997cdee7b7bcd189bacc36fb4edc76a76da225
KumudKode <raj94971@gmail.com>
2025-04-03 21:16:41 +0530

    memory of js

---
+++ Day-wise/basics/06_Memory.js
@@ -0,0 +1,9 @@
+//memory is a crucial aspect of programming, as it determines how data is stored and accessed in a program.
+// Stack, heap and memory management are important concepts in programming that help manage memory efficiently and effectively.
+// Stack memory is used for static memory allocation, while heap memory is used for dynamic memory allocation.
+// Stack memory is faster than heap memory, but has limited size and is used for storing local variables and function calls.
+// Heap memory is slower than stack memory, but has larger size and is used for storing objects and data structures.
+// Memory management is the process of allocating and deallocating memory in a program to optimize performance and prevent memory leaks.
+// It involves techniques such as garbage collection, reference counting, and memory pooling to manage memory efficiently and effectively.
+// Creating visual representations of stack and heap memory
+
